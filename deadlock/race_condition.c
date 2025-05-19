ac35d770dfc904b5f9e3df1aff73e83354ab438e
kawsar_ahmmed_hridoy <kawsarhridoy0146@gmail.com>
2025-05-20 03:14:29 +0600

    Just entered deadlock

---
+++ deadlock/race_condition.c
@@ -0,0 +1,42 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<unistd.h>
+int sharedMemory=1;
+void* thread1(){
+    int x=sharedMemory;
+    printf("Current value of shared memory: %d\n",x);
+    x++;
+    printf("Current value of register: %d\n",x);
+    sleep(1);
+    sharedMemory=x;
+    printf("Current value of shared memory: %d\n",sharedMemory);
+    pthread_exit(NULL);
+}
+
+void* thread2(){
+    int x=sharedMemory;
+    printf("Current value of shared memory: %d\n",x);
+    x--;
+    printf("Current value of register: %d\n",x);
+    sleep(1);
+    sharedMemory=x;
+    printf("Current value of shared memory: %d\n",sharedMemory);
+    pthread_exit(NULL);
+}
+
+int main(){
+
+    printf("Main thread.\n");
+    pthread_t th1,th2;
+
+    pthread_create(&th1, NULL, &thread1, NULL);
+    pthread_create(&th2, NULL, &thread2, NULL);
+    
+    pthread_join(th1, NULL);
+    pthread_join(th2, NULL);
+
+    printf("Final value of shared memory is: %d\n", sharedMemory);
+
+    return 0;
+}
