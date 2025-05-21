fda401d2579e98ac498e85fd97596a2cf2e24256
kawsar_ahmmed_hridoy <kawsarhridoy0146@gmail.com>
2025-05-21 10:09:53 +0600

    added rename logic

--- shell_scripting/create_file.sh
+++ shell_scripting/create_file.sh
@@ -2,3 +2,8 @@ echo "Enter the name of your file: "
 read name
 touch $name
 echo "File created successfully!"
+echo "Rename the file X file into Y"
+read X
+read Y
+mv "$X" "$Y"
+echo "File renamed to '$Y' successfully!"
